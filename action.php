23483aa057a871693fcaa298f6449ac8284c1d5c
if-zimops-then-if-then-else <77450249+if-zimops-then-if-then-else@users.noreply.github.com>
2021-11-08 14:21:52 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,44 +1,10 @@
-<?php
- 
-if($_POST) {
-    $Name = "";
-    $Email = "";
-    $Message = "";
-    $email_body = "<div>";
-     
-    if(isset($_POST['Name'])) {
-        $Name = filter_var($_POST['Name'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Name:</b></label>&nbsp;<span>".$Name."</span>
-                        </div>";
-    }
-
-    if(isset($_POST['Email'])) {
-        $Email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['Email']);
-        $Email = filter_var($Email, FILTER_VALIDATE_EMAIL);
-        $email_body .= "<div>
-                           <label><b>Email:</b></label>&nbsp;<span>".$Email."</span>
-                        </div>";
-    }
-     
-    if(isset($_POST['Message'])) {
-        $Message = htmlspecialchars($_POST['Message']);
-        $email_body .= "<div>
-                           <label><b>Message:</b></label>
-                           <div>".$Message."</div>
-                        </div>";
-    }
-    $recipient = "simsch538@gmail.com";
-    $email_body .= "</div>";
-
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $Email . "\r\n";
-     
-    if(mail($recipient, $email_body, $headers)) {
-    }
-     
-} else {
-    echo '<p>Something went wrong</p>';
-}
-?> Read More: Create a Contact Form in PHP
+<?php $name = $_POST['Name'];
+$email = $_POST['Email'];
+$message = $_POST['Message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "simsch538@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
